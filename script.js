7da36b08ac5a48b93e2bd9d322df9b11f31aaef4
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2022-12-18 08:23:03 +0000

    Defination of Hoisting, Spread and Destructing

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
+//**Spread**
+//[Array]
+let arr = ["string", 43, false, ["nextStr", 34, true], { name: "chikki", class: 3, "WannaStudy": false }]
+console.log(arr)
+let collectNewInArr = ["mango", ...arr]//Use to Duplicate => Array,Object
+console.log(collectNewInArr)
+//{Object}
+let obj = {
+  id: 1,
+  name: 'Ben',
+  about: { 'name': 'Leslie' }
+}
+console.log(obj)
+let collect = {
+  'programmer': false
+}
+let allObj = { ...obj, ...collect }
+console.log(allObj)
 
-//Normal Function
-// function greet() {
-//   let name = prompt(`What's Your Name`)
-//   alert(`Good Morning ${name} `)
-// }
-// greet()
-//IIFE
-(function(d) {
-  let name = prompt(`What's Your Name`)
-  if (d.getHours() > 12 && d.getHours() < 17) {
-    alert(`Good AfterNoon ${name}`)
-  } else if (d.getHours() > 16) {
-    alert(`Good Evening ${name} `)
-  } else {
-    alert(`Good Morning ${name} `)
-  }
-})(new Date())
+//**Destructuring**
+let fruits = ["mango", "banana", "apple", true, 32]
+// let [mango,apple,banana]= fruits
+let [mango, , apple, banana] = fruits//apple = apple
+//**Hoisting**
+birthday()
+function birthday() {
+  console.log(b)
+  var b = `happy birthday`//undefiend
+  // let b = `happy birthday`//error
+}
